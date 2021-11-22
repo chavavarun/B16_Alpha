7f9a2ca417cc546acb551edfa71f42aca4f717be
chavavarun <chava.varun@gmail.com>
2021-11-22 12:28:45 +0530

    cart items added

---
+++ B16ABookKart/src/app/secure/cart-items-page/cart-items-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-items-page',
+  templateUrl: './cart-items-page.component.html',
+  styleUrls: ['./cart-items-page.component.scss']
+})
+export class CartItemsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
